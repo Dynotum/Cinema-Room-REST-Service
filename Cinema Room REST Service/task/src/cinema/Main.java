8378af088a0d6b2e75ce0b8936be9fe9446aa4aa
Carlos Sedano <carlos.sedano@oracle.com>
2021-12-09 16:16:01 -0600

    add gitignore

--- Cinema Room REST Service/task/src/cinema/Main.java
+++ Cinema Room REST Service/task/src/cinema/Main.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hola spring!");
         SpringApplication.run(Main.class, args);
     }
+
 }
